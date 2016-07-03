96a50f10e8d1e115a0d1818172bb02a1e8e6c6a1
NaifousBS <soufian1993@hotmail.fr>
2016-07-03 16:00:29 +0200

    Favicon ajouté

--- header.php
+++ header.php
@@ -5,16 +5,19 @@
 	<meta name="author" content="">
 
 	<title>OPCIFORME - S'orienter, se construire, s'accomplir.</title>
+	<link rel="icon" type="image/png" href="assets/images/favicon.png" />
 
 	<!-- CSS -->
 	<link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
 	<link href="assets/css/font-awesome.min.css" rel="stylesheet" media="screen">
 	<link href="assets/css/simple-line-icons.css" rel="stylesheet" media="screen">
 	<link href="assets/css/animate.css" rel="stylesheet">
+
     
 	<!-- Custom styles CSS -->
 	<link href="assets/css/style.css" rel="stylesheet" media="screen">
     
     <script src="assets/js/modernizr.custom.js"></script>
+
        
 </head>
